740213dc1400b785c68001007fb3b521ee2bc34f
Damla Nisa CEVIK <damlanisa.cevik@gmail.com>
2020-03-10 13:33:19 +0300

    Start a django project called "hipo_recipes"

---
+++ hipo_recipes/hipo_recipes/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for hipo_recipes project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hipo_recipes.settings')
+
+application = get_wsgi_application()
