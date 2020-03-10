3bec78d9edf992a0b9c719f2bc803177f195b90d
Damla Nisa CEVIK <damlanisa.cevik@gmail.com>
2020-03-10 21:02:05 +0300

    Change settings.py and admin.py, add migrations

--- hipo_recipes/hipo_recipes/settings.py
+++ hipo_recipes/hipo_recipes/settings.py
@@ -32,8 +32,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'users.apps.UsersConfig'
+    'users.apps.UsersConfig',
     'recipes.apps.RecipesConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,3 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'recipes-home'
+LOGIN_URL = 'login'
