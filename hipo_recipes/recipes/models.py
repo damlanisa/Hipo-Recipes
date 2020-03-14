cba44c50854450effac73206f7e47488857040c7
Damla Nisa CEVIK <damlanisa.cevik@gmail.com>
2020-03-14 16:11:00 +0300

    Add Recipe model

--- hipo_recipes/recipes/models.py
+++ hipo_recipes/recipes/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('recipe-detail', kwargs={'pk': self.pk})
