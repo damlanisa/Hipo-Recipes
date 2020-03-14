4cba0bafcab5f7e72d0bb91743966b8e733845e0
Damla Nisa CEVIK <damlanisa.cevik@gmail.com>
2020-03-14 16:12:46 +0300

    Add RecipeListView, RecipeDetailView, RecipeCreateView, RecipeUpdateView, RecipeDeleteView

--- hipo_recipes/recipes/views.py
+++ hipo_recipes/recipes/views.py
@@ -1,18 +1,63 @@
 from django.shortcuts import render
-
-
-posts = [
-    {
-        'author': 'Damla',
-        'title': 'Recipe 1',
-        'content': 'First recipe content',
-        'date_posted': 'March 10, 2020'
-    },
-]
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+from .models import Recipe
 
 
 def home(request):
     context = {
-        'posts': posts
+        'recipes': Recipe.objects.all()
     }
     return render(request, 'recipes/home.html', context)
+
+
+class RecipeListView(ListView):
+    model = Recipe
+    template_name = 'recipes/home.html'
+    context_object_name = 'recipes'
+    ordering = ['-date_posted']
+
+
+class RecipeDetailView(DetailView):
+    model = Recipe
+
+
+class RecipeCreateView(LoginRequiredMixin, CreateView):
+    model = Recipe
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class RecipeUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Recipe
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        recipe = self.get_object()
+        if self.request.user == recipe.author:
+            return True
+        return False
+
+
+class RecipeDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Recipe
+    success_url = '/'
+
+    def test_func(self):
+        recipe = self.get_object()
+        if self.request.user == recipe.author:
+            return True
+        return False
