c7889eb853a9103ee2c8ee14a054dd43c67206bd
Damla Nisa CEVIK <damlanisa.cevik@gmail.com>
2020-03-18 12:29:04 +0300

    Add Like and Rate models

--- hipo_recipes/recipes/models.py
+++ hipo_recipes/recipes/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Ingredient(models.Model):
@@ -30,3 +31,29 @@ class Recipe(models.Model):
 
     def get_absolute_url(self):
         return reverse('recipe-detail', kwargs={'pk': self.pk})
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, related_name='likes', on_delete=models.CASCADE)
+    is_liked = models.BooleanField(default=False)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.user) + ':' + str(self.recipe) + ':' + str(self.is_liked)
+
+    class Meta:
+        unique_together = ("user", "recipe")
+
+
+class Rate(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, related_name='rates', on_delete=models.CASCADE)
+    points = models.IntegerField(default=1, validators=[MaxValueValidator(5), MinValueValidator(1)])
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.user) + ':' + str(self.recipe) + ':' + str(self.rate)
+
+    class Meta:
+        unique_together = ("user", "recipe")
